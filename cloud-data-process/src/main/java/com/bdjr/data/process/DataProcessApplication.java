0cb1989bb0318469a4652f65c3fdb07d9ea57cc2
cyoung <cy880708@163.com>
2019-01-30 16:05:06 +0800

    Initial commit

---
+++ cloud-data-process/src/main/java/com/bdjr/data/process/DataProcessApplication.java
@@ -0,0 +1,21 @@
+package com.bdjr.data.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-general
+ * @package: com.bdjr.data.process、
+ * @email: cy880708@163.com
+ * @date: 2019/1/30 下午3:56
+ * @mofified By:
+ */
+@SpringBootApplication
+public class DataProcessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataProcessApplication.class, args);
+    }
+}
---
+++ cloud-data-process/src/main/java/com/bdjr/data/process/controller/CompanyController.java
@@ -0,0 +1,26 @@
+package com.bdjr.data.process.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-general
+ * @package: com.bdjr.data.process.controller、
+ * @email: cy880708@163.com
+ * @date: 2019/1/30 下午4:00
+ * @mofified By:
+ */
+@RestController
+public class CompanyController {
+
+    @RequestMapping(value = "/insertCompany", method = RequestMethod.GET)
+    public String insertCompany() {
+
+        
+
+    }
+}
